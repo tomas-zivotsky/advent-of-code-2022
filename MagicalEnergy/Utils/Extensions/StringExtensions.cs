f43ba45518fc5d7ee78de8dc93e1d0701ae548a7
Tomáš Životský <zivotsky.t@gmail.com>
2023-01-01 11:06:33 +0100

    utils project

---
+++ MagicalEnergy/Utils/Extensions/StringExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Utils.Extensions
+{
+    public static class StringExtensions
+    {
+        public static bool IsNullOrEmpty(this string? value) => string.IsNullOrEmpty(value);
+
+        public static bool IsNullOrWhiteSpace(this string? value) => string.IsNullOrEmpty(value);
+    }
+}
