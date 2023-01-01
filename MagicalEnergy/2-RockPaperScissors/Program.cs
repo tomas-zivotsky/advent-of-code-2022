a14272e7b0399222cc17a91b1b46d0e0f2468cbe
Tomáš Životský <zivotsky.t@gmail.com>
2023-01-01 20:06:02 +0100

    supporting another interpretation of the input file

--- MagicalEnergy/2-RockPaperScissors/Program.cs
+++ MagicalEnergy/2-RockPaperScissors/Program.cs
@@ -1,5 +1,6 @@
 ﻿using _2_RockPaperScissors;
 using Microsoft.Extensions.Configuration;
+using Utils.Helpers;
 
 var builder = new ConfigurationBuilder()
     .AddJsonFile("appsettings.json", true, true);
@@ -10,17 +11,29 @@ var path = args.Length >= 1
     ? args.First()
     : config["Input:DefaultPath"] ?? throw new ArgumentException("Default path not found.");
 
-var parser = new InputParser();
-var guide = parser.Parse(path);
+var fileHelper = new FileHelper();
 
+var parser = new InputParser(fileHelper);
 var evaluator = new Evaluator();
-int totalScore = 0;
 
-foreach ((Choice choice, Response response) in guide)
+var guideResponses = parser.Parse<Choice, Response>(path);
+
+int totalScore = 0;
+foreach ((Choice choice, Response response) in guideResponses)
 {
     totalScore += evaluator.Evaluate(choice, response);
 }
 
-Console.WriteLine($"Total score is: {totalScore}.");
+Console.WriteLine($"#1: Total score is: {totalScore}.");
+
+var guideResults = parser.Parse<Choice, Result>(path);
+
+totalScore = 0;
+foreach ((Choice choice, Result result) in guideResults)
+{
+    totalScore += evaluator.Evaluate(choice, result);
+}
+
+Console.WriteLine($"#2: Total score is: {totalScore}.");
 
 Console.ReadKey();
