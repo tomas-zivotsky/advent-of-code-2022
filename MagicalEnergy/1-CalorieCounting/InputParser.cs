b4b84cb475a33ed55b772406b30c5fe5ffda8ae2
Tomáš Životský <zivotsky.t@gmail.com>
2023-01-01 17:13:45 +0100

    parsing input file

---
+++ MagicalEnergy/1-CalorieCounting/InputParser.cs
@@ -0,0 +1,35 @@
+﻿using Utils.Extensions;
+
+namespace _1_CalorieCounting;
+
+public class InputParser
+{
+    public IEnumerable<Inventory> Parse(string path)
+    {
+        if (path is null) throw new ArgumentNullException(nameof(path));
+        if (!File.Exists(path)) throw new ArgumentException($"File {path} not found.");
+
+        Inventory? current = null;
+
+        foreach (var line in File.ReadLines(path))
+        {
+            if (line.IsNullOrWhiteSpace())
+            {
+                if (current is not null)
+                {
+                    yield return current;
+                }
+
+                current = new();
+                continue;
+            }
+
+            current ??= new();
+
+            if (!int.TryParse(line, out var calories))
+                throw new ArgumentException($"Invalid input format: '{line}' is not an integer.");
+
+            current.Food.Add(new(calories));
+        }
+    }
+}
