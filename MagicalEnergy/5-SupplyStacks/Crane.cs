69f45ccc8b39ce8261ee3ef6c3e37927255313a4
Tomáš Životský <zivotsky.t@gmail.com>
2023-01-11 01:08:02 +0100

    adding support for CrateMover 9001

--- MagicalEnergy/5-SupplyStacks/Crane.cs
+++ MagicalEnergy/5-SupplyStacks/Crane.cs
@@ -1,13 +1,17 @@
 ﻿namespace _5_SupplyStacks;
 
-internal class Crane
+internal abstract class Crane
 {
+    public virtual string Name => "Just a crane";
+
+    protected abstract bool IsAbleToPickMore { get; }
+
     public IEnumerable<Crate> DoWork(IEnumerable<Instruction> instructions, CrateStack[] stacks)
     {
         foreach (var instruction in instructions)
         {
             var taken = stacks[instruction.From - 1].PopFirst(instruction.Count);
-            stacks[instruction.To - 1].AddFirst(taken);
+            stacks[instruction.To - 1].AddFirst(taken, IsAbleToPickMore);
         }
 
         foreach (var stack in stacks)
--- MagicalEnergy/5-SupplyStacks/Program.cs
+++ MagicalEnergy/5-SupplyStacks/Program.cs
@@ -16,14 +16,18 @@ var fileHelper = new FileHelper();
 var parser = new InputParser(fileHelper);
 
 int parsedLines = 0;
-CrateStack[] stacks = parser.ParseCrateStacks(path, ref parsedLines);
+CrateStack[] stacks9000 = parser.ParseCrateStacks(path, ref parsedLines);
+parsedLines = 0;
+CrateStack[] stacks9001 = parser.ParseCrateStacks(path, ref parsedLines);
 
-IEnumerable<Instruction> instructions = parser.ParseInstructions(path, parsedLines);
+IEnumerable<Instruction> instructions = parser.ParseInstructions(path, parsedLines).ToArray();
 
-var crane = new Crane();
+var crateMover9000 = new CrateMover9000();
+IEnumerable<Crate> result9000 = crateMover9000.DoWork(instructions, stacks9000);
+Console.WriteLine($"Using {crateMover9000.Name}: the crates on top of each stack are: '{string.Join("", result9000.Select(crate => crate.Value))}'");
 
-IEnumerable<Crate> result = crane.DoWork(instructions, stacks);
-
-Console.WriteLine($"The crates on top of each stack are: '{string.Join("", result.Select(crate => crate.Value))}'");
+var crateMover9001 = new CrateMover9001();
+IEnumerable<Crate> result9001 = crateMover9001.DoWork(instructions, stacks9001);
+Console.WriteLine($"Using {crateMover9001.Name}: the crates on top of each stack are: '{string.Join("", result9001.Select(crate => crate.Value))}'");
 
 Console.ReadKey();
