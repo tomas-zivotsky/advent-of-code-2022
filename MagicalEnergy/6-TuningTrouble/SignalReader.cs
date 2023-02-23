8d9d1beb749c46fb0b647015d2a4aea1680e29a8
Tomáš Životský <zivotsky.t@gmail.com>
2023-02-23 22:29:11 +0100

    reading signal buffer

---
+++ MagicalEnergy/6-TuningTrouble/SignalReader.cs
@@ -0,0 +1,32 @@
+﻿using Utils.Collections;
+
+namespace _6_TuningTrouble;
+
+internal class SignalReader
+{
+    private readonly IEnumerable<char> _signals;
+
+    public SignalReader(IEnumerable<char> signals)
+    {
+        _signals = signals ?? throw new ArgumentNullException(nameof(signals));
+    }
+
+
+    public int FindOffset(int sequenceCount)
+    {
+        var queue = new LimitedQueue<char>(sequenceCount);
+
+        int index = 0;
+        foreach (char signal in _signals)
+        {
+            queue.Enqueue(signal);
+            index++;
+
+            if (queue.Count < queue.Limit) continue;
+
+            if (queue.Distinct().Count() == queue.Limit) return index;
+        }
+
+        return index;
+    }
+}
